dfd333e7ffde663b4ab39a3f8374b1ea2f7cbb4c
Theo Weller <weller.theodor@gmail.com>
2019-03-12 15:19:55 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,28 @@
+require 'pry'
+class Person
+  def initialize(name)
+    @name = name
+  end
+
+  def name=(new_name)
+    @name = new_name
+  end
+
+  def name
+    @name
+  end
+
+  def initialize(job)
+    @job = job
+  end
+
+  def job=(new_job)
+    @job = new_job
+  end
+
+  def job
+    @job
+  end
+  
+end
+#binding.pry
